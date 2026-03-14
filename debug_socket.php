50d226c653382c4b641c521435b9e3224f2d04dd
edagrex-eng <ed.ag.rex@gmail.com>
2026-03-14 17:58:35 -0600

    archivos sin uso borrados

--- debug_socket.php
+++
@@ -1,8 +0,0 @@
-<?php
-$socket = stream_socket_server("tcp://0.0.0.0:8080", $errno, $errstr);
-if (!$socket) {
-    echo "ERROR: $errno - $errstr\n";
-} else {
-    echo "Listening on 8080...\n";
-    fclose($socket);
-}
